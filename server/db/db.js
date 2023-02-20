1e0b82e7f5f44f439af9405dac737666387205e3
SameerShetty <kalyanshettisameer@gmail.com>
2023-02-20 23:17:01 +0530

    cyclic lastlast

--- server/db/db.js
+++ server/db/db.js
@@ -6,6 +6,7 @@ const connectDb = async () => {
     const conn = mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      socketTimeoutMS: 30000,
     });
     console.log("MONGODB is connected".cyan.underline.bold);
   } catch (error) {
