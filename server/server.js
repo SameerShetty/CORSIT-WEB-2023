68551e40487006a88e8a6038ca61cb0e1cbb43ab
SameerShetty <kalyanshettisameer@gmail.com>
2023-02-20 23:37:12 +0530

    cyclic lastestlast

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 const path = require("path");
 const connectDb = require("./db/db");
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "150mb" }));
 app.use(cors());
 app.use(morgan("dev"));
 
