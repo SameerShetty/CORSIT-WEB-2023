f977a3fcb94965b3dc37235472feb43488a57201
Sameer Kumar Kalyanshetti <95131876+SameerShetty@users.noreply.github.com>
2023-02-21 17:33:09 +0530

    Update db.jd

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     mongoose.set("strictQuery", true);
-    const conn = mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       connectTimeoutMS: 30000,
@@ -15,3 +15,4 @@ const connectDb = async () => {
 };
 
 module.exports = connectDb;
+
